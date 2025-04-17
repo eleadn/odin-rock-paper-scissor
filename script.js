a5a89fed41a11f91b488a733e2d8e49e4e531e87
eleadn <eleano.dn@gmail.com>
2025-04-17 20:42:27 +0200

    Add computer logic

---
+++ script.js
@@ -0,0 +1,17 @@
+function getComputerChoice()
+{
+    let choice = Math.random();
+
+    if (choice < 0.33)
+    {
+        return "rock";
+    }
+    else if (choice < 0.66)
+    {
+        return "paper";
+    }
+    else
+    {
+        return "scissor";
+    }
+}
