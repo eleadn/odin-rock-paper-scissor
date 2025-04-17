8653f8d81d08f8ef6986d834e57eb147508fb09f
eleadn <eleano.dn@gmail.com>
2025-04-17 20:59:32 +0200

    Add rounds

--- script.js
+++ script.js
@@ -21,5 +21,35 @@ function getHumanChoice()
     return prompt("Choose rock, Paper, or scissor : ")
 }
 
+function playRound(humanChoice, computerChoice)
+{
+    humanChoice = humanChoice.toLowerCase();
+    const result = `${humanChoice} vs ${computerChoice} : `;
+
+    if (humanChoice === computerChoice)
+    {
+        console.log(result + "it's a draw.")
+        return;
+    }
+
+    if (humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "scissor" && computerChoice === "paper" ||
+        humanChoice === "rock" && computerChoice === "scissor")
+    {
+        console.log(result + "you win !")
+        ++humanScore;
+    }
+    else
+    {
+        console.log(result + "you lose...")
+        ++computerScore;
+    }
+}
+
 let humanScore = 0;
 let computerScore = 0;
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
