5e348dcadacc17827a364e0f67dc47cfdb17c184
eleadn <eleano.dn@gmail.com>
2025-04-19 17:04:35 +0200

    Add UI

--- script.js
+++ script.js
@@ -16,19 +16,10 @@ function getComputerChoice()
     }
 }
 
-function getHumanChoice()
-{
-    return prompt("Choose rock, Paper, or scissor : ")
-}
-
 function playRound(humanChoice, computerChoice)
 {
-    humanChoice = humanChoice.toLowerCase();
-    const result = `${humanChoice} vs ${computerChoice} : `;
-
     if (humanChoice === computerChoice)
     {
-        console.log(result + "it's a draw.")
         return 0;
     }
 
@@ -36,45 +27,98 @@ function playRound(humanChoice, computerChoice)
         humanChoice === "scissor" && computerChoice === "paper" ||
         humanChoice === "rock" && computerChoice === "scissor")
     {
-        console.log(result + "you win !")
         return 1;
     }
     else
     {
-        console.log(result + "you lose...")
         return -1;
     }
 }
 
-function playGame()
+function endGame()
 {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < 5; ++i)
+    if (humanScore === 5)
     {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-
-        switch (playRound(humanSelection, computerSelection))
-        {
-            case 1:
-                ++humanScore;
-                break;
-            case -1:
-                ++computerScore;
-                break;
-        }
-    }
-
-    if (humanScore > computerScore)
-    {
-        console.log("Congrats ! You won !");
+        alert("Good job ! You won !");
     }
     else
     {
-        console.log("Sorry, you lost...");
+        alert("Sorry, you lose...");
+    }
+
+    humanScore = 0;
+    computerScore = 0;
+    initDisplays();
+}
+
+function onChoiceButtonClick(button)
+{
+    const computerChoice = getComputerChoice();
+    const humanChoice = button.id.slice(1, button.id.length);
+    const result = playRound(humanChoice, computerChoice);
+    updateRoundResult(result, humanChoice, computerChoice);
+    if (humanScore >= winningScore || computerScore >= winningScore)
+    {
+        endGame();
     }
 }
 
-playGame();
+function updateRoundResult(result, humanChoice = "", computerChoice = "")
+{
+    const resultText = `${humanChoice} vs ${computerChoice} : `;
+
+    switch (result)
+    {
+        case 1:
+            resultDisplay.textContent = resultText + "You win !";
+            ++humanScore;
+            updateHumanScore();
+            break;
+        case -1:
+            resultDisplay.textContent = resultText + "You lose...";
+            ++computerScore;
+            updateComputerScore();
+            break
+        case 0:
+            resultDisplay.textContent = resultText + "It's a draw.";
+            break;
+        default:
+            resultDisplay.textContent = "...";
+            break;
+    }
+}
+
+function updateHumanScore()
+{
+    humanScoreDisplay.textContent = `Your score: ${humanScore}`;
+}
+
+function updateComputerScore()
+{
+    computerScoreDisplay.textContent = `Computer score: ${computerScore}`;
+}
+
+function initChoiceButton(button)
+{
+    button.addEventListener("click", context => onChoiceButtonClick(context.target));
+}
+
+function initDisplays()
+{
+    updateRoundResult();
+    updateHumanScore();
+    updateComputerScore();
+}
+
+const buttons = document.querySelectorAll(".choiceButton");
+const humanScoreDisplay = document.querySelector(".humanScore");
+const computerScoreDisplay = document.querySelector(".computerScore");
+const resultDisplay = document.querySelector(".roundResult");
+
+let humanScore = 0;
+let computerScore = 0;
+
+const winningScore = 5;
+
+buttons.forEach(button => initChoiceButton(button));
+initDisplays();
